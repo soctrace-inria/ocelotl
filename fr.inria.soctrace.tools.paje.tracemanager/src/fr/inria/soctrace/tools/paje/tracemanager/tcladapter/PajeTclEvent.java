85cc6f478b2b86ab6957d5fccc949fecf06dd499
Damien Dosimont <dosimont@localhost.inf.ufrgs.br>
2013-11-19 15:33:00 -0200

    paje header updated

--- fr.inria.soctrace.tools.paje.tracemanager/src/fr/inria/soctrace/tools/paje/tracemanager/tcladapter/PajeTclEvent.java
+++ fr.inria.soctrace.tools.paje.tracemanager/src/fr/inria/soctrace/tools/paje/tracemanager/tcladapter/PajeTclEvent.java
@@ -1,3 +1,21 @@
+/* ===========================================================
+ * Paje Trace Manager module
+ * =====================================================================
+ * 
+ * This module is a FrameSoC plug in which enables to import, export and
+ * process Pajé trace files
+ *
+ * (C) Copyright 2013 INRIA
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Damien Dosimont <damien.dosimont@imag.fr>
+ *     Generoso Pagano <generoso.pagano@inria.fr>
+ */
 
 package fr.inria.soctrace.tools.paje.tracemanager.tcladapter;
 
