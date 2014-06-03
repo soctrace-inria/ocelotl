8d1578e831385d0d20c2a6792e638afab8ca39b1
youenn <youenn.corre@inria.fr>
2014-06-03 16:53:19 +0200

    added new class for default parameters

---
+++ fr.inria.soctrace.tools.ocelotl.ui/src/fr/inria/soctrace/tools/ocelotl/ui/views/OcelotlDefaultParameterConstants.java
@@ -0,0 +1,43 @@
+package fr.inria.soctrace.tools.ocelotl.ui.views;
+/*
+ * Class for the constants defining the default parameters in Ocelotl
+ */
+
+
+public class OcelotlDefaultParameterConstants {
+	
+	/**
+	 * Precision Threshold
+	 */
+	public static final String Threshold = "0.001";
+	
+	/**
+	 * Starting timestamp
+	 */
+	public static final String TimestampStart = "0";
+	
+	/**
+	 * Ending timestamp
+	 */
+	public static final String TimestampEnd = "0";
+	
+	/**
+	 * Should the quality be normalized ?
+	 */
+	public static final boolean Normalize = false;
+	
+	/**
+	 * Should the curves show increasing quality ?
+	 */
+	public static final boolean GrowingQualities = true;
+	
+	/**
+	 * Number of time slices
+	 */
+	public static final int TimeSliceNumber = 100;
+	
+	/**
+	 * Gain/Loss ratio
+	 */
+	public static final String RunParameter = "1.0";
+}
