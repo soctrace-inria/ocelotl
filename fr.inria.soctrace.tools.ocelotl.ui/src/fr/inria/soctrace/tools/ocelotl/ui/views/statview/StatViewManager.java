118681d6a01589d2c2361de2892ab558117cda24
Damien Dosimont <damien.dosimont@imag.fr>
2014-11-03 12:27:50 +0100

    statopframe

---
+++ fr.inria.soctrace.tools.ocelotl.ui/src/fr/inria/soctrace/tools/ocelotl/ui/views/statview/StatViewManager.java
@@ -0,0 +1,65 @@
+/* =====================================================================
+ * Ocelotl Visualization Tool
+ * =====================================================================
+ * 
+ * Ocelotl is a FrameSoC plug in that enables to visualize a trace 
+ * overview by using aggregation techniques
+ *
+ * (C) Copyright 2013 INRIA
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Damien Dosimont <damien.dosimont@imag.fr>
+ *     Generoso Pagano <generoso.pagano@inria.fr>
+ */
+
+package fr.inria.soctrace.tools.ocelotl.ui.views.statview;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.runtime.Platform;
+import org.osgi.framework.Bundle;
+
+import fr.inria.soctrace.tools.ocelotl.ui.views.OcelotlView;
+
+public class StatViewManager {
+
+	OcelotlView	ocelotlView;
+
+	public StatViewManager(final OcelotlView ocelotlView) {
+		super();
+		this.ocelotlView = ocelotlView;
+	}
+
+	public IStatView create() {
+		IStatView timeLineView = null;
+		try {
+			final Bundle mybundle = Platform.getBundle(ocelotlView.getCore().getStatOperators().getSelectedOperatorResource().getBundle());
+			timeLineView = (IStatView) mybundle.loadClass(ocelotlView.getCore().getStatOperators().getSelectedOperatorResource().getVisualization()).getDeclaredConstructor(OcelotlView.class).newInstance(ocelotlView);
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return timeLineView;
+
+	}
+	
+	public IStatView create(String aVisualization) {
+		IStatView timeLineView = null;
+		try {
+			final Bundle mybundle = Platform.getBundle(ocelotlView.getCore().getStatOperators().getSelectedOperatorResource(aVisualization).getBundle());
+			timeLineView = (IStatView) mybundle.loadClass(ocelotlView.getCore().getStatOperators().getSelectedOperatorResource(aVisualization).getVisualization()).getDeclaredConstructor(OcelotlView.class).newInstance(ocelotlView);
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return timeLineView;
+
+	}
+
+
+}
