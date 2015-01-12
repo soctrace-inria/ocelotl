5a9824d18121a38f9f4e1e0a958aa78a7e090297
ycorre <youenn.corre@inria.fr>
2015-01-12 10:17:08 +0100

    Added compilation script for the lpaggreg library (issue #99)

---
+++ fr.inria.lpaggreg/compilationScript/win/lpaggreg_win32.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+ARCH=$1
+BASE="buildLpaggreg"
+LPAGGREG_SRC_DIR=$2/src
+LPAGGREGJNI_SRC_DIR=$3/src
+JNI_INCLUDE_DIR=$4
+
+if [ "$ARCH" = "32" ]; then
+  echo "Compiling for windows x86"
+  BUILD="build_win32_x86"
+  COMP=i686-w64-mingw32-g++
+else 
+  echo "Compiling for windows x64"
+  BUILD="build_win32_x64"
+  COMP=x86_64-w64-mingw32-g++
+fi
+
+mkdir -p $BASE/tmp/
+mkdir -p $BASE/$BUILD/
+
+LPA_NEWREP=$BASE/tmp/lpaggreg_`eval date +%Y%m%d%H%M%S`
+LPAJNI_NEWREP=$BASE/tmp/lpaggregjni_`eval date +%Y%m%d%H%M%S`
+
+cp -R $LPAGGREG_SRC_DIR $LPA_NEWREP
+cp -R $LPAGGREGJNI_SRC_DIR $LPAJNI_NEWREP
+cp -R $LPAGGREG_SRC_DIR $LPAJNI_NEWREP/lpaggreg
+
+cd $LPA_NEWREP
+
+echo "Building liblpaggreg.a..."
+$COMP -O1 -Wall -c *.cpp
+ar rs liblpaggreg.a *.o
+
+cp liblpaggreg.a ../../../$LPAJNI_NEWREP
+
+cd ../../../$LPAJNI_NEWREP
+
+echo "Building lpaggreg.dll..."
+$COMP -c lpaggreg_wrap.cxx -I $JNI_INCLUDE_DIR -I .
+
+$COMP -Wl,-s -shared -Wl,-subsystem,windows -mthreads -Wl,--out-implib,liblpaggreg.a -o lpaggregjni.dll lpaggreg_wrap.o -D_JNI_IMPLEMENTATION_ -Wl,--kill-at -static -Wl,--whole-archive -L . -l lpaggreg -Wl,--no-whole-archive
+
+cp lpaggregjni.dll ../../$BUILD
+
+cd ../..
+
+echo "Cleaning temp files..."
+rm -R tmp	
+
+
+
+
