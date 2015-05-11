98c3acf436530b5348c31ccfa557ceb9938aa4fa
Damien Dosimont <damien.dosimont@imag.fr>
2015-05-11 14:28:25 +0200

    adding new script for compilation

---
+++ fr.inria.soctrace.tools.ocelotl.maven.master/upload_ocelotl.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+                                                                                        
+#####################################################################
+# Upload Ocelotl update site.
+# 
+# This is simply a convenience shortcut for the upload-site.sh script.
+#
+# IMPORTANT
+#
+# It works having the framesoc and the soctrace-inria.github.io 
+# clones in the same root directory:
+# ./somedir/ocelotl
+# ./somedir/soctrace-inria.github.io
+#
+# Original Author: Generoso Pagano
+# Adaptation to Ocelotl: Damien Dosimont
+#####################################################################
+
+SCRIPT="../../soctrace-inria.github.io/updatesite/upload-site.sh"
+REPO="../fr.inria.soctrace.tools.ocelotl.maven.repository/target/repository/"
+PROJECT="ocelotl"
+$SCRIPT $REPO $PROJECT
